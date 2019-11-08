95891cefc329e6b86aeb34df58cda052f4299a51
Dan <daniel.moore.3187@gmail.com>
2019-11-08 00:59:46 -0700

    A better responsive menu on every page

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
-function menu() {
-	var x = document.getElementById("js-menu");
-	if (x.className === "nl") {
-		x.className += "responsive";
+function newMenu() {
+	if (document.getElementById("new-menu").innerHTML === "") {
+		document.getElementById("new-menu").innerHTML =
+		'<a href="index.html">Home</a><br /><a href="about.html">About</a><br /><a href="portfolio.html">Portfolio</a><br /><a href="resume.html">Resume</a><br /><a href="contact.html">Contact</a>'
 	} else {
-		x.classname = "nl";
+		document.getElementById("new-menu").innerHTML = ""
 	}
 }
