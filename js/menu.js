44e1440fa4056c8a5c8d2bb2a015ba23d89a5ae3
Dan <daniel.moore.3187@gmail.com>
2019-11-11 21:14:31 -0700

    Mostly just comments

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,4 @@
+// Information for the menu without any of the formatting
 var menuText = ['Home', 'About', 'Portfolio', 'Resume', 'Contact'];
 var menuLinks = ['index', 'about', 'portfolio', 'resume', 'contact'];
 var menuItems = [];
@@ -7,6 +8,8 @@ for(var i=0; i < 5; i++) {
 	menuItems[i].innerText = menuText[i];
 }
 
+// Make the desktop menu by default without adding any extra newlines
+// Only do this if the screen is wide enough
 function createMenu() {
 	var menu = document.getElementById('top-nav');
 	if(!window.matchMedia("(max-width: 600px)").matches) {
@@ -16,10 +19,11 @@ function createMenu() {
 	}
 }
 
+// Make a new vertical menu (adding newlines) when the mobile menu expand icon
+// is clicked
 function newMenu() {
 	var dropMenu = document.getElementById("new-menu");
 	if (dropMenu.innerHTML === "") {
-		//document.getElementById('hamburger').className = 'icon-clear';
 		document.getElementById('top-nav').classList.toggle('morphed');
 		for(var i=0; i<5; i++) {
 			dropMenu.appendChild(menuItems[i]);
@@ -27,7 +31,6 @@ function newMenu() {
 		}
 	} else {
 		document.getElementById('top-nav').classList.toggle('morphed');
-		//document.getElementById('hamburger').className = 'icon-menu';
 		dropMenu.innerHTML = ""
 	}
 }
